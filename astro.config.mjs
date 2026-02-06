143fee5c2814bab2642383d555c4acf1dc71ae15
Idris Hasanaj <idris@Idriss-Mac-mini.fritz.box>
2026-02-06 10:51:04 +0100

    feat: GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://idris-web.github.io',
+  base: '/mulk-30',
   vite: {
     plugins: [tailwindcss()],
     preview: {
